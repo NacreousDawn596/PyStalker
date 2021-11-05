b7edd6b29d27a2b53e2b8d56c913cf0a31de8bcf
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-11-05 23:08:02 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+echo "downloading files"
+sleep 1
+sudo apt-get install python3 python3-pip
+clear
+sudo pacman -S python3 python3-pip
+clear
+sudo dnf install python3 python3-pip
+clear
+pip install requests
+clear
+echo "everything is ready, you can launch it by typing 'python3 main.py random_username'. enjoy!"
